b52d8814e02408dcc7eb60e006a8423063587dfb
Bruno Abreu <bruno.souza2001@gmail.com>
2023-08-15 23:16:21 -0300

    Index and Functions JS

--- JS/index.js
+++ JS/index.js
@@ -9,6 +9,7 @@ document.addEventListener('DOMContentLoaded', function () {
         document.getElementById('ip').textContent = data.ip;
         document.getElementById('plataforma').textContent = data.plataforma;
         document.getElementById('path_arquivo').textContent = data.path_arquivo;
+        document.getElementById('sha256').textContent = data.sha256;
         document.getElementById('nome_arquivo').textContent = data.nome_arquivo;
         document.getElementById('nome_host').textContent = data.nome_host;
         document.getElementById('descricao').textContent = data.descricao;
@@ -33,3 +34,28 @@ function saveData(event) {
         window.open(nextPage, '_blank'); 
     }
 }
+
+const btnCopiar = document.getElementById('btnCopiar');
+    
+btnCopiar.addEventListener('click', () => {
+
+    const conteudoCenter = document.querySelector('center').innerHTML;
+
+  const tempDiv = document.createElement('div');
+  tempDiv.innerHTML = conteudoCenter;
+
+  document.body.appendChild(tempDiv);
+
+  const range = document.createRange();
+  range.selectNodeContents(tempDiv);
+  const selection = window.getSelection();
+  selection.removeAllRanges();
+  selection.addRange(range);
+
+  document.execCommand('copy');
+
+  document.body.removeChild(tempDiv);
+
+  alert('Conteúdo copiado para a área de transferência!');
+});
+
